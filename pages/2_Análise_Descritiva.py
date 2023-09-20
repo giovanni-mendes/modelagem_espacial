17ffe66642df7613bb4274341a72cef3dfff3b9a
Giovanni Mendes <134141396+giovanni-mendes@users.noreply.github.com>
2023-09-19 22:36:43 -0300

    Update 2_Análise_Descritiva.py

--- pages/2_Análise_Descritiva.py
+++ pages/2_Análise_Descritiva.py
@@ -2,20 +2,6 @@ import streamlit as st
 import pandas as pd
 import io
 
-st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url(https://i.imgur.com/UxsOhf4.png
-                background-repeat: no-repeat;
-                padding-top: 250px;
-                background-position: 20px 20px;
-            }
-  
-        </style>
-        """,
-        unsafe_allow_html=True,
-)
 
 
 @st.cache_data
