f77c86cf93592a07556ce1d0350a4022db9c43eb
Giovanni Mendes <134141396+giovanni-mendes@users.noreply.github.com>
2023-09-19 22:49:20 -0300

    Update 4_I_de_Moran.py

--- pages/4_I_de_Moran.py
+++ pages/4_I_de_Moran.py
@@ -65,11 +65,3 @@ with st.expander("Veja a Explicação"):
     """)
 
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
