1b444ce51648fc7bacd0c11746a7905c77a0b230
Giovanni Mendes <134141396+giovanni-mendes@users.noreply.github.com>
2023-09-20 07:53:20 -0300

    Update 4_I_de_Moran.py

--- pages/4_I_de_Moran.py
+++ pages/4_I_de_Moran.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="Modelagem Espacial",
+    page_icon="🗺️",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+
 st.markdown(
         """
         <style>
