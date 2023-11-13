fa231c2247fb345083218705f8c9d9dd3c3d7dc8
Giovanni Mendes <134141396+giovanni-mendes@users.noreply.github.com>
2023-11-13 09:27:59 -0300

    Update 4_I_de_Moran.py

--- pages/4_I_de_Moran.py
+++ pages/4_I_de_Moran.py
@@ -65,7 +65,7 @@ df['desenvolvimento'] = np.select(lista_condicoes, lista_escolha)
 
 
 st.markdown(
-    'Describe IFDM 2016')
+    'Describe IFDM 2016 para o Brasil')
 anos = list(df['Ano'].unique())
 st.write(df['IFDM'].loc[(df['Ano'] == anos[-1]) & (df['IFDM'] > 0)].describe())
 
@@ -165,6 +165,42 @@ with st.expander("Veja a Explicação"):
 
         Por fim, há outliers espaciais do tipo HL no Norte/Nordeste do país. 
     """)
+
+
+st.write('#I de Moran para o Minas Gerais')
+
+mg = df.loc[df['UF'] == 'MG']
+st.markdown(
+    'Describe IFDM 2016 para Minas Gerais')
+
+st.write(mg['IFDM'].loc[(mg['Ano'] == anos[-1]) & (mg['IFDM'] > 0)].describe())
+
+with st.expander("Veja a Interpretação"):
+    st.write("""
+        - Observações para cerca de 5.441 municípios (IFDM positivo);
+        - A média do índice IFDM para o ano de 2016 foi de 0.6678, ou seja, em média, o desenvolvimento brasileiro é moderado;
+        - O valor mínimo de desenvolvimento foi de 0.3214 do município de Ipixuna no Amazonas.
+        - O valor máximo de desenvolvimento foi de 0.9006 do município de Louveira em São Paulo.
+    """)
+
+st.write('## Proporção de Desenvolvimento Municipal em 2016')
+
+fig =  px.pie(mg.loc[df['Ano']=='2016'], 
+       names='desenvolvimento',
+       hole = 0.5,
+       color='desenvolvimento',
+        width=600,
+        height=600,
+       color_discrete_map={'Alto': 'green', 
+                           'Moderado':'#04033d', 
+                           'Regular': '#700270',
+                           'Baixo':'red'})
+
+fig.update_layout(annotations=[dict(text='Desenvolvimento', x=0.5, y=0.5, font_size=20, showarrow=False)])
+st.plotly_chart(fig, use_container_width=True)
+
+
+
 
 hide_st_style = """
             <style>
