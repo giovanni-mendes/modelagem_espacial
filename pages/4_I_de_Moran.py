728855b85e389c9c7ef358f80c6cf48c66ba53a4
Giovanni Mendes <134141396+giovanni-mendes@users.noreply.github.com>
2023-11-17 16:00:20 -0300

    Update 4_I_de_Moran.py

--- pages/4_I_de_Moran.py
+++ pages/4_I_de_Moran.py
@@ -95,7 +95,7 @@ fig =  px.pie(df.loc[df['Ano'] == anos[-1]],
 fig.update_layout(annotations=[dict(text='Desenvolvimento', x=0.5, y=0.5, font_size=20, showarrow=False)])
 st.plotly_chart(fig, use_container_width=True)
 
-df = df.loc[df['Ano] == '2016']
+df = df.loc[df['Ano] ==  anos[-1]]
 fig = px.histogram(df.loc[df['IFDM'] > 0], 
                    x="IFDM",
                    marginal="box",
